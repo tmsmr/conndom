ed7b3a496004fe72e0fa617b6590b84d972dbbb8
Thomas Maier <contact@thomas-maier.net>
2025-11-23 10:30:48 +0100

    ✨ add and delete networks

--- control/conndomd/internal/pkg/wifi/connections.go
+++ control/conndomd/internal/pkg/wifi/connections.go
@@ -1,5 +1,7 @@
 package wifi
 
+import "github.com/google/uuid"
+
 type ConfiguredNetwork struct {
 	ID   string
 	UUID string
@@ -27,4 +29,57 @@ func (m *Manager) ListConfiguredNetworks() ([]ConfiguredNetwork, error) {
 		})
 	}
 	return networks, nil
+}
+
+func (m *Manager) AddConfiguredNetwork(ssid string, passphrase string) (*ConfiguredNetwork, error) {
+	settings := map[string]map[string]interface{}{
+		"connection": {
+			"id":             ssid,
+			"uuid":           uuid.NewString(),
+			"type":           "802-11-wireless",
+			"interface-name": m.conf.StaWifiInterface,
+			"autoconnect":    true,
+		},
+		"802-11-wireless": {
+			"mode": "infrastructure",
+			"ssid": []uint8(ssid),
+		},
+		"ipv4": {
+			"method": "auto",
+		},
+		"ipv6": {
+			"method": "ignore",
+		},
+	}
+	if passphrase != "" {
+		settings["802-11-wireless-security"] = map[string]interface{}{
+			"key-mgmt": "wpa-psk",
+			"psk":      passphrase,
+		}
+	}
+	conn, err := m.settings.AddConnection(settings)
+	if err != nil {
+		return nil, err
+	}
+	settingsConn, err := conn.GetSettings()
+	if err != nil {
+		return nil, err
+	}
+	return &ConfiguredNetwork{
+		ID:   settingsConn["connection"]["id"].(string),
+		UUID: settingsConn["connection"]["uuid"].(string),
+		SSID: string(settingsConn["802-11-wireless"]["ssid"].([]uint8)),
+	}, nil
+}
+
+func (m *Manager) DeleteConfiguredNetwork(uuid string) error {
+	conn, err := m.settings.GetConnectionByUUID(uuid)
+	if err != nil {
+		return err
+	}
+	err = conn.Delete()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- control/conndomd/internal/pkg/wifi/manager_integration_test.go
+++ control/conndomd/internal/pkg/wifi/manager_integration_test.go
@@ -52,3 +52,22 @@ func TestManager_ListConnections(t *testing.T) {
 	}
 	_, err = m.ListConfiguredNetworks()
 }
+
+func TestManager_AddDeleteConfiguredNetwork(t *testing.T) {
+	conf, err := env.Load()
+	if err != nil {
+		t.Fatal(err)
+	}
+	m, err := wifi.NewManager(*conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	network, err := m.AddConfiguredNetwork("test-ssid", "test-passphrase")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = m.DeleteConfiguredNetwork(network.UUID)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
